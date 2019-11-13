3ebaab180f539b82153c1c5185d607a78f45b6c7
Joseph_Tully <tully4school@gmail.com>
2019-11-13 14:32:31 -0500

    successfuly able to loop up which drinks have alcohol and which do not, along with a name search

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const parser = require("body-parser");
 const Drink = require("./lib/models/Mixer");
 
 app.use(parser.json());
-
-app.get("/drinks", function(req, res) {
-	Drink.find({}).then(drinks => res.json(drinks));
-});
+app.use(require("./lib/routes/index"));
 
 app.listen(3000, () => console.log("listening for mixers on port 3000"));
