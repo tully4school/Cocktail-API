8b3d37d4e31d35cdd3730f17936cf8c442f8fe3c
Joseph_Tully <tully4school@gmail.com>
2019-11-14 04:42:29 -0500

    adding swagger to the project files to begin documentation

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 const parser = require("body-parser");
-const Drink = require("./lib/models/Mixer");
 
 app.use(parser.json());
 app.use(require("./lib/routes/index"));
-
+app.post("/drinks/", function(req, res) {
+	Mixer.create(req.body).then(drink => {
+		res.json(drink);
+	});
+});
+app.put("/drinks/:id", function(req, res) {
+	Mixer.findOneAndUpdate({ _id: req.params.id }, req.body, {
+		new: true
+	}).then(drink => {
+		res.json(drink);
+	});
+});
+app.delete("/drinks/:id", function(req, res) {
+	Mixer.findOneAndRemove({ _id: req.params.id }).then(drink => {
+		res.json(drink);
+	});
+});
 app.listen(3000, () => console.log("listening for mixers on port 3000"));
