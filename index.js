a77d37a2fa24e126758b654f5cf56d7a3c7b3205
Joseph_Tully <tully4school@gmail.com>
2019-11-14 19:10:15 -0500

    about to push the api live

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const parser = require("body-parser");
+const cors = require("cors");
 
+app.use(cors());
 app.use(parser.json());
+
 app.use(require("./lib/routes/index"));
 app.post("/drinks/", function(req, res) {
 	Mixer.create(req.body).then(drink => {
